5f08bfd64557177090f820739bfe218ce9b24100
alsekaram <git@awl.su>
2025-07-31 19:46:37 +0300

    Remove unused external and database-related files
    Deleted several files related to the external nonce implementation and database management, including interfaces, factories, and database connection logic. This cleanup reduces clutter and improves project maintainability by removing obsolete code.

--- src/alienworlds/external/nonce/interfaces.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class NonceGenerator(ABC):
-    @abstractmethod
-    async def get_nonce(self, account: str, last_mine_trx: str, difficulty: int) -> str:
-        pass
